02e52b90510633c1a3d609f442cd5ed60b73ad72
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-09 11:29:32 -0600

    Delete routes.js

--- billing/routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("../controllers/billing.controller");
-
-router.post("/portal", controller.createPortal);
-router.get("/customer", controller.getCustomer);
-router.post("/cancel", controller.cancelSubscription);
-
-module.exports = router;
