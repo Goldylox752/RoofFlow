8ba6b93da232918208f2a5e74df865bf88392ab0
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-09 09:47:56 -0600

    Delete portal.routes.js

--- portal.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const controller = require("./portal.controller");
-
-router.post("/create", controller.createPortalSession);
-router.get("/customer", controller.getCustomer);
-router.post("/cancel", controller.cancelSubscription);
-
-module.exports = router;
