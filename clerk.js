90bfe2836e264101fd2509315a8c00af35475c20
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-14 17:09:27 -0600

    Create clerk.js

---
+++ clerk.js
@@ -0,0 +1,9 @@
+// lib/clerk.js
+
+const { createClerkClient } = require("@clerk/backend");
+
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
+
+module.exports = clerkClient;
