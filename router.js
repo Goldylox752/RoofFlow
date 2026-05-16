5a1520daa4a14d99cf0c817893d0ead1daabe009
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-16 12:25:32 -0600

    Delete router.js

--- router.js
+++
@@ -1,7 +0,0 @@
-function routeLead(lead) {
-  if (lead.score > 80) return "email_priority";
-  if (lead.score > 50) return "email_standard";
-  return "nurture";
-}
-
-module.exports = { routeLead };
