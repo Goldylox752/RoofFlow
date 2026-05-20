394fcb5aece84ed88bd553b9b6f5656b3cfd85b6
Byron Sanche <261788158+Goldylox752@users.noreply.github.com>
2026-05-20 13:18:34 -0600

    Delete posthog.ts

--- lib/analytics/posthog.ts
+++
@@ -1,9 +0,0 @@
-import posthog from "posthog-js";
-
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "https://app.posthog.com",
-});
-
-export const track = (event: string, props?: any) => {
-  posthog.capture(event, props);
-};
